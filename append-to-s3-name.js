80d536bd8d0d1b656a8afc5b7afb876bab1548fb
Felipe <felipe.delatorre@fivetalent.com>
2019-12-09 08:12:45 -0800

    remove lodash. add uuid

--- append-to-s3-name.js
+++ append-to-s3-name.js
@@ -1,24 +1,21 @@
-'use strict'
-const _ = require('lodash')
 const uuidv4 = require('uuid/v4');
 
 class AppendToS3Name {
-    constructor(serverless, options) {
-        this.hooks = {
-          'before:package:finalize': () => appenduuid(serverless)
-        }
-    }
-}
-
-
-const appenduuid = serverless => {
-
-  if (serverless.service.resources.Resources) {
-      _.forEach(serverless.service.resources.Resources, (resourcesObj, resourcesName) => {
-          if(resourcesObj.Type === 'AWS::S3::Bucket'){
-              serverless.service.resources.Resources[resourcesName].Properties.BucketName += `-${uuidv4()}`;
-          }
-      })
+  constructor(serverless, options) {
+    this.hooks = {
+      'before:package:finalize': () => appenduuid(serverless),
+    };
   }
 }
-module.exports = AppendToS3Name
+
+const appenduuid = serverless => {
+  if (serverless.service.resources.Resources) {
+    for (const resourceName in serverless.service.resources.Resources) {
+      if (serverless.service.resources.Resources[resourceName].Type === 'AWS::S3::Bucket') {
+        serverless.service.resources.Resources[resourceName].Properties.BucketName += `-${uuidv4()}`;
+      }
+    }
+  }
+};
+
+module.exports = AppendToS3Name;
